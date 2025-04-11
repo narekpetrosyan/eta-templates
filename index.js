8c0066932f233a27443866d1516b8b823a4ce96b
narekpetrosyan <narek.petrosyan988@gmail.com>
2025-04-11 15:13:29 +0400

    redesigned templates

--- index.js
+++ index.js
@@ -11,16 +11,49 @@ const eta = new Eta({
 });
 
 app.get("/", async (req, res) => {
+  // const renderedTemplate = await eta.renderAsync(
+  //   "contact-support.template.eta",
+  //   {
+  //     title: "Hello some big title",
+  //     supportEmail: "support@example.com",
+  //     email: "test@example.com",
+  //     category: "Support",
+  //     isDriver: true,
+  //     platformUsed: ["Uber", "Bolt"],
+  //     message: "Hello, this is a test message",
+  //   }
+  // );
+
+  // const renderedTemplate = await eta.renderAsync(
+  //   "profile-review-requested.template.eta",
+  //   {
+  //     title: "Hello some big title",
+  //     userProfileUrl: `123/1213`,
+  //     fullName: "Full name",
+  //   }
+  // );
+
+  // const renderedTemplate = await eta.renderAsync("sign-up-email.template.eta", {
+  //   title: "Hello some big title",
+  //   userProfileUrl: `123/1213`,
+  //   fullName: "Full name",
+  // });
+
   const renderedTemplate = await eta.renderAsync(
-    "contact-support.template.eta",
+    "profile-updated-admin-notification.template.eta",
     {
-      title: "Hello some big title",
-      supportEmail: "support@example.com",
-      email: "test@example.com",
-      category: "Support",
-      isDriver: true,
-      platformUsed: ["Uber", "Bolt"],
-      message: "Hello, this is a test message",
+      updatedFields: [
+        {
+          key: "name",
+          from: "Old name",
+          to: "Uopdated",
+        },
+        {
+          key: "lat name",
+          from: "last name",
+          to: "Updated last name",
+        },
+      ],
     }
   );
 
